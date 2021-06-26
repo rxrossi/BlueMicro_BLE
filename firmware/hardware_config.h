aa4ffa2721f40654166ed3c2f5f83b846299c0d0
Alex Ignez <rx.rossi@gmail.com>
2021-06-26 20:28:39 +0100

    Switch to dactyl manuform 5x6, bluemicro 2.1, left

--- firmware/hardware_config.h
+++ firmware/hardware_config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2018-2021 <Pierre Constantineau>
+Copyright 2020-2021 <Pierre Constantineau>
 
 3-Clause BSD License
 
@@ -23,23 +23,43 @@ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR P
 
 /* HARDWARE DEFINITION*/
 /* key matrix size */
-#define MATRIX_ROWS 4
-#define MATRIX_COLS 7
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 6
 
-#define MATRIX_ROW_PINS {13, 24, 9, 10 }
-#define MATRIX_COL_PINS {26, 29, 2, 45, 3, 28, 43 }
+#define MATRIX_ROW_PINS {3, 2, 12, 14, 13, 11 } // { F6, F7, B1, B3, B2, B6 }
+#define MATRIX_COL_PINS {27, 28, 29, 30, 15, 16 } // { D4, C6, D7, E6, B4, B5 }
 #define UNUSED_PINS {}
-#define BACKLIGHT_LED_PIN 38 
+
+/* Commented because LEDs are not used. Pin 16 is conflicting with matrix mapping
+#define BACKLIGHT_LED_PIN 16 
 #define DEFAULT_PWM_VALUE 10000            // PWM intensity
-#define BACKLIGHT_PWM_ON 1
+#define BACKLIGHT_PWM_ON 1  
+*/
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
-#define BATTERY_TYPE BATT_LIPO
-#define VBAT_PIN  31
-#define VCC_PIN 12
-#define VCC_POLARITY_ON 1
-
-#define  STATUS_BLE_LED_PIN  19  //blue = 0.19
+     #define BATTERY_TYPE BATT_LIPO
+        #define VBAT_PIN  31
+        #define  STATUS_BLE_LED_PIN  19  //blue = 0.19
 #define  STATUS_KB_LED_PIN 17  //red = 0.17
+    /*    #define D3      6  //
+        #define D2      8   //
+        #define D1      25  //sda
+        #define D0      26  //scl
+        #define D4      27
+        #define C6      28
+        #define D7      29
+        #define E6      30
+        #define B4      15
+        #define B5      16
+
+        #define F4      5
+        #define F5      4
+        #define F6      3
+        #define F7      2
+        #define B1      12  //sck
+        #define B3      14  //miso
+        #define B2      13  //mosi
+        #define B6      11
+        #define NC      24 */
     
 #endif /* HARDWARE_CONFIG_H */
