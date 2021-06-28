2f194a937216edaec028f3e67d62ad4753bcdbaf
Alex Ignez <rx.rossi@gmail.com>
2021-06-28 17:26:15 +0100

    Update Left side keymaps

--- firmware/keymap.cpp
+++ firmware/keymap.cpp
@@ -55,37 +55,36 @@ void process_user_layers(uint16_t layermask)
 #if KEYBOARD_SIDE == LEFT
 
 /* Qwerty
- * ,-----------------------------------------.
- * | Esc  |   1  |   2  |   3  |   4  |   5  | 
- * |-----------------------------------------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  | 
- * |------+------+------+------+------+------|
- * | Shift|   A  |   S  |   D  |   F  |   G  |
- * |------+------+------+------+------+------|
- * | Ctrl |   Z  |   X  |   C  |   V  |   B  |
- * `------+------+------+------+------+------'
- *               |   [  |   ]  |
- *               `---------------------------.
- *                             |Lower |Space |
- *                             `-------------------------. 
- *                                           | Tab | Home|
- *                                           ------------| 
- *                                           | BSpc|  `  |
- *                                           ------------' 
+ * ,------------------------------------------------.
+ * | Esc  |   1  |   2  |   3  |   4  |   5  |   6  | 
+ * |------------------------------------------------|
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   [  |
+ * |------+------+------+------+------+-------------|
+ * | CAPS |   A  |   S  |   D  |   F  |   G  |      |
+ * |------+------+------+------+------+-------------'
+ * |SHIFT |   Z  |   X  |   C  |   V  |   B  |
+ * |------+------+------+------+------+-------
+ * | CTRL |      |      |      |Lower |Space |
+ * `------+------+------+---------------------------. 
+ *                                      | Tab | Home|
+ *                                      |-----|-----| 
+ *                                      | BSpc|  `  |
+ *                                      `-----------' 
  */
 
 std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix =
     {KEYMAP(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, 
-        KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,
-        KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
-        _______, _______, KC_LBRC, KC_RBRC, L_LOWER, KC_SPC,
-                                            KC_TAB,  KC_HOME,
-                                            KC_BSPC, KC_GRAVE
+        KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,
+        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, 
+        KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,
+        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
+        KC_LCTRL, _______, _______, KC_RBRC, L_LOWER, KC_SPC,
+
+                                             KC_TAB,  KC_HOME,
+                                             KC_BSPC, KC_GRAVE
     )};
 
- 
+     
 void setupKeymap() {
 
 
